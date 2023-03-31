0749124f8ecf26c9a27bbdc220a5e5763d5ec0e7
Remigiusz Mikolajewicz <r.mikolajewicz@interia.pl>
2023-03-31 16:12:18 +0200

    add global file for api keys

--- store/news/thunk.ts
+++ store/news/thunk.ts
@@ -1,15 +1,14 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 import { RootState } from "../store";
-
-const API_KEY = process.env.NEXT_PUBLIC_API_KEY_NEWS;
+import { API_KEY_NEWS } from "public/apiKeys";
 
 export const fetchNewsData = createAsyncThunk(
   "news/fetchNews",
   async (_, { getState }) => {
     const country = (getState() as RootState).news.currentCountry;
     const response = await axios.get(
-      `https://newsapi.org/v2/everything?q=${country}&pageSize=25&apiKey=${API_KEY}`
+      `https://newsapi.org/v2/everything?q=${country}&pageSize=25&apiKey=${API_KEY_NEWS}`
     );
     return response.data;
   }
